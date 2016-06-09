594a74458a276b0aaedbbcadac40cfb1841c902a
kiran4399 <kiran4399@gmail.com>
2016-06-09 22:01:27 +0530

    MPU 6050 overlay recreated and added the entry in compile.sh

--- install_files/compile_overlay.sh
+++ install_files/compile_overlay.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-MPU_OVERLAY="BBBMPU6500-00A0"
+MPU6050_OVERLAY="BBBMPU6050-00A0"
+MPU6500_OVERLAY="BBBMPU6500-00A0"
 BMP_OVERLAY="BBBBMP280-00A0"
 
-echo "Installing $MPU_OVERLAY Device Tree Overlay"
-dtc -O dtb -o /lib/firmware/$MPU_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU_OVERLAY.dts
+echo "Installing $MPU6500_OVERLAY Device Tree Overlay"
+dtc -O dtb -o /lib/firmware/$MPU_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6500_OVERLAY.dts
 
+echo "Installing $MPU6050_OVERLAY Device Tree Overlay"
+dtc -O dtb -o /lib/firmware/$BMP_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6050_OVERLAY.dts
 
 echo "Installing $BMP_OVERLAY Device Tree Overlay"
 dtc -O dtb -o /lib/firmware/$BMP_OVERLAY.dtbo -b 0 -@ 2016-05-01/$BMP_OVERLAY.dts
