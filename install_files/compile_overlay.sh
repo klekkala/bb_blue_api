d5a25addc94cf2b20483e1ac79c27e8d2f557e24
kiran4399 <kiran4399@gmail.com>
2016-06-09 22:05:23 +0530

    silly bug removed

--- install_files/compile_overlay.sh
+++ install_files/compile_overlay.sh
@@ -5,10 +5,10 @@ MPU6500_OVERLAY="BBBMPU6500-00A0"
 BMP_OVERLAY="BBBBMP280-00A0"
 
 echo "Installing $MPU6500_OVERLAY Device Tree Overlay"
-dtc -O dtb -o /lib/firmware/$MPU_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6500_OVERLAY.dts
+dtc -O dtb -o /lib/firmware/$MPU6500_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6500_OVERLAY.dts
 
 echo "Installing $MPU6050_OVERLAY Device Tree Overlay"
-dtc -O dtb -o /lib/firmware/$BMP_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6050_OVERLAY.dts
+dtc -O dtb -o /lib/firmware/$MPU6050_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6050_OVERLAY.dts
 
 echo "Installing $BMP_OVERLAY Device Tree Overlay"
 dtc -O dtb -o /lib/firmware/$BMP_OVERLAY.dtbo -b 0 -@ 2016-05-01/$BMP_OVERLAY.dts
