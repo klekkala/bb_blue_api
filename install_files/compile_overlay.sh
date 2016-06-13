1587a975cbb73c3e6d6f61982d8998c2637c08d2
kiran4399 <kiran4399@gmail.com>
2016-06-13 22:31:37 +0530

    compile file updated

--- install_files/compile_overlay.sh
+++ install_files/compile_overlay.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-MPU6050_OVERLAY="BBBMPU6050-00A0"
-MPU6500_OVERLAY="BBBMPU6500-00A0"
-BMP_OVERLAY="BBBBMP280-00A0"
-
-echo "Installing $MPU6500_OVERLAY Device Tree Overlay"
-dtc -O dtb -o /lib/firmware/$MPU6500_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6500_OVERLAY.dts
+MPU6050_OVERLAY="BBB_MPU-00A0"
+BMP_OVERLAY="BBB_BMP280-00A0"
+BUTLED_OVERLAY="BBB_BUTLED-00A0"
 
 echo "Installing $MPU6050_OVERLAY Device Tree Overlay"
 dtc -O dtb -o /lib/firmware/$MPU6050_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6050_OVERLAY.dts
@@ -13,4 +10,6 @@ dtc -O dtb -o /lib/firmware/$MPU6050_OVERLAY.dtbo -b 0 -@ 2016-05-01/$MPU6050_OV
 echo "Installing $BMP_OVERLAY Device Tree Overlay"
 dtc -O dtb -o /lib/firmware/$BMP_OVERLAY.dtbo -b 0 -@ 2016-05-01/$BMP_OVERLAY.dts
 
+echo "Installing $BUTLED_OVERLAY Device Tree Overlay"
+dtc -O dtb -o /lib/firmware/$BUTLED_OVERLAY.dtbo -b 0 -@ 2016-05-01/$BUTLED_OVERLAY.dts
 
