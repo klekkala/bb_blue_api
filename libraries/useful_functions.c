b37d1c21794fad94c4ff834db90d407661d0f0cd
kiran4399 <kiran4399@gmail.com>
2016-06-20 19:20:47 +0530

    other functions updated

--- libraries/useful_functions.c
+++ libraries/useful_functions.c
@@ -5,7 +5,7 @@
 * robotics cape library. These do not necessarily interact with hardware.
 *******************************************************************************/
 
-#include "../bb_blue_api.h"
+#include "../robotics_cape.h"
 #include "../useful_includes.h"
 
 /*******************************************************************************
@@ -16,6 +16,16 @@
 *******************************************************************************/
 int null_func(){
 	return 0;
+}
+
+/*******************************************************************************
+* float get_random_float()
+*
+* returns a random floating point number between -1 and 1
+*******************************************************************************/
+float get_random_float(){
+	unsigned int a = (rand()&0x007fffff) | 0x40000000;
+    return (*((float*)&a) - 3.0f);
 }
 
 /*******************************************************************************
@@ -193,6 +203,33 @@ int suppress_stderr(int (*func)(void)){
 
     return ret;
 }
+
+/*******************************************************************************
+* continue_or_quit()
+*
+* This is a blocking function which returns 0 if the user presses ENTER.
+* otherwise it returns -1 on any other keypress. If ctrl-C is pressed it will
+* additionally set the global state to EXITITING. This is useful for checking
+* if the user wishes to continue with a process or quit.
+*******************************************************************************/
+int continue_or_quit(){
+	// set stdin to non-canonical raw mode to capture all button presses 
+	system("stty raw");
+	
+  	int c=getchar();
+  	int ret = -1; // set to 0 if enter is pressed
+	if(c==3){
+		printf("recieved Ctrl-C\n");
+		set_state(EXITING);
+	}else if(c=='\r' || c=='\n'){
+		ret=0;
+	}
+	// put stdin back to normal canonical mode
+	system ("stty cooked");
+	printf("\n");
+	return ret;
+}
+
 
 
 
