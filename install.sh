973c11cf487f9e5a8506d01f4159ddb0a514071a
Kiran Kumar Lekkala <kiran4399@gmail.com>
2016-10-05 23:59:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Bash script to install supporting software for the Robotics Cape
 # tested on follwing beagleboard.org Debian releases 
 # 2014-05-14, 2015-03-01, 2015-11-12
-# Modified by Kiran Kumar Lekkala and Will be used for building and installing bb_blue api on Beaglebone Black
+# Modified by Kiran Kumar Lekkala and will be used for building and installing bb_blue api on Beaglebone Black
 
 INSTALL_DIR="/root"
 BOOTSCRIPT="Auto_Run_Script.sh"
